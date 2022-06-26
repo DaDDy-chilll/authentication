d1b24090bc86cc18fa053c9d68f2423211f7ac62
DaDDy <blacklistuser420@gmail.com>
2022-06-26 15:02:00 +0630

    security level 1 finish

---
+++ secret/app.js
@@ -0,0 +1,64 @@
+//jshint esversion:6
+const express = require("express");
+const ejs = require("ejs");
+const bdparser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bdparser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
+const userSchema = {
+  email: String,
+  password: String,
+};
+const User = new mongoose.model("User", userSchema);
+app.get("/", function (req, res) {
+  res.render("home");
+});
+
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+
+app.post("/login", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({ email: username }, function (err, found) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (found) {
+        if (found.password === password) {
+          res.render("secrets");
+        }
+      }
+    }
+  });
+});
+
+app.get("/register", function (req, res) {
+  res.render("register");
+});
+
+app.post("/register", function (req, res) {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+  newUser.save(function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("secrets");
+    }
+  });
+});
+
+app.listen(3000, function () {
+  console.log("Server is running on port:3000");
+});
